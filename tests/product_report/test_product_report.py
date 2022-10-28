d4912c4104bb5634902afe2be5e70239b2db36b7
Tiemi Faustino <hayashipharma@gmail.com>
2022-10-28 15:31:59 -0300

    test: 2 - Implements test of individual product report

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        2,
+        "Ração Golden - gatos castrados frango",
+        "PremieR Pet",
+        "02-05-2022",
+        "02-05-2024",
+        "123456",
+        "em local seco, fresco e arejado, ao abrigo da luz",
+    )
+    assert (
+        str(product) == "O produto Ração Golden - gatos castrados frango"
+        " fabricado em 02-05-2022"
+        " por PremieR Pet com validade"
+        " até 02-05-2024"
+        " precisa ser armazenado em local seco, fresco e arejado,"
+        " ao abrigo da luz."
+    )
