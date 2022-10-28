00c1aa5515094dc9bd2fc462149ebdb9099826b3
Tiemi Faustino <hayashipharma@gmail.com>
2022-10-28 15:08:35 -0300

    feat: :test_tube: Implements test of the Product object constructor/initiator

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,23 @@
-# from inventory_report.inventory.product import Product - iniciando projeto
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        2,
+        "Ração Golden - gatos castrados frango",
+        "PremieR Pet",
+        "2022-05-02",
+        "2024-05-02",
+        "123456",
+        "Conservar em local seco, fresco e arejado, ao abrigo da luz",
+    )
+    assert product.id == 2
+    assert product.nome_do_produto == "Ração Golden - gatos castrados frango"
+    assert product.nome_da_empresa == "PremieR Pet"
+    assert product.data_de_fabricacao == "2022-05-02"
+    assert product.data_de_validade == "2024-05-02"
+    assert product.numero_de_serie == "123456"
+    assert (
+        product.instrucoes_de_armazenamento
+        == "Conservar em local seco, fresco e arejado, ao abrigo da luz"
+    )
