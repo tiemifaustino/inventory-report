b03e733f6871a325a6729e22ae761f2537de8b9d
Tiemi Faustino <hayashipharma@gmail.com>
2022-10-28 15:15:48 -0300

    refactor: :recycle: Change test date of test_cria_produto function

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -6,16 +6,16 @@ def test_cria_produto():
         2,
         "Ração Golden - gatos castrados frango",
         "PremieR Pet",
-        "2022-05-02",
-        "2024-05-02",
+        "02-05-2022",
+        "02-05-2024",
         "123456",
         "Conservar em local seco, fresco e arejado, ao abrigo da luz",
     )
     assert product.id == 2
     assert product.nome_do_produto == "Ração Golden - gatos castrados frango"
     assert product.nome_da_empresa == "PremieR Pet"
-    assert product.data_de_fabricacao == "2022-05-02"
-    assert product.data_de_validade == "2024-05-02"
+    assert product.data_de_fabricacao == "02-05-2022"
+    assert product.data_de_validade == "02-05-2024"
     assert product.numero_de_serie == "123456"
     assert (
         product.instrucoes_de_armazenamento
