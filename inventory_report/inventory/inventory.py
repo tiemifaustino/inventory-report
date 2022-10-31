b9722d571716690753aa0b4e7424e0fe4e5a09d6
Tiemi Faustino <hayashipharma@gmail.com>
2022-10-31 17:31:53 -0300

    feat: :white_check_mark: 7 - Generate the reports through a XML file

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,5 +1,7 @@
 import csv
 import json
+import xmltodict
+
 
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
@@ -9,22 +11,30 @@ from pathlib import Path
 class Inventory:
     @staticmethod
     def import_data(path, type):
-        product_list = []
-        file_path_suffix = Path(path).suffix
-
-        if file_path_suffix == ".csv":
-            with open(path) as file:
-                read_file = csv.DictReader(file)
-                product_list = [product for product in read_file]
-        if file_path_suffix == ".json":
-            with open(path) as file:
-                read_file = file.read()
-                product_list = json.loads(read_file)
+        product_list = Inventory.verify_suffix(path)
 
         if type == "simples":
             return SimpleReport.generate(product_list)
         if type == "completo":
             return CompleteReport.generate(product_list)
+
+    @classmethod
+    def verify_suffix(cls, path):
+        product_list = []
+        file_path_suffix = Path(path).suffix
+
+        with open(path) as file:
+            if file_path_suffix == ".csv":
+                read_file = csv.DictReader(file)
+                product_list = [product for product in read_file]
+            if file_path_suffix == ".json":
+                read_file = file.read()
+                product_list = json.loads(read_file)
+            if file_path_suffix == ".xml":
+                read_file = file.read()
+                xml_dict = xmltodict.parse(read_file)
+                product_list = xml_dict["dataset"]["record"]
+        return product_list
 
 
 """
