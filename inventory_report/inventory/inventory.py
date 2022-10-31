8ec06e498c90a267bb1f3fd927cb6a019efaeaca
Tiemi Faustino <hayashipharma@gmail.com>
2022-10-31 16:09:13 -0300

    feat: :white_check_mark: 6 - Generate the reports through a JSON file

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,15 +1,25 @@
 import csv
+import json
 
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
+from pathlib import Path
 
 
 class Inventory:
     @staticmethod
     def import_data(path, type):
-        with open(path) as file:
-            read_file = csv.DictReader(file)
-            product_list = [product for product in read_file]
+        product_list = []
+        file_path_suffix = Path(path).suffix
+
+        if file_path_suffix == ".csv":
+            with open(path) as file:
+                read_file = csv.DictReader(file)
+                product_list = [product for product in read_file]
+        if file_path_suffix == ".json":
+            with open(path) as file:
+                read_file = file.read()
+                product_list = json.loads(read_file)
 
         if type == "simples":
             return SimpleReport.generate(product_list)
