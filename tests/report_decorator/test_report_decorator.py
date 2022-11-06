c9271b7d9a192a6a7d32c2f4672471abcc2ffc93
Tiemi Faustino <hayashipharma@gmail.com>
2022-11-06 16:22:42 -0300

    feat: :white_check_mark: 9 - Tests the generation of a color version of the report

--- tests/report_decorator/test_report_decorator.py
+++ tests/report_decorator/test_report_decorator.py
@@ -1,5 +1,46 @@
-# from inventory_report.reports.colored_report import ColoredReport
+from inventory_report.reports.colored_report import ColoredReport
+from inventory_report.reports.simple_report import SimpleReport
 
 
 def test_decorar_relatorio():
-    pass  # Seu teste deve ser escrito aqui
+    simple_report_test = ColoredReport(SimpleReport).generate(
+        [
+            {
+                "id": 1,
+                "nome_do_produto": "MESA",
+                "nome_da_empresa": "Forces of Nature",
+                "data_de_fabricacao": "2022-05-04",
+                "data_de_validade": "2023-02-09",
+                "numero_de_serie": "FR48",
+                "instrucoes_de_armazenamento": "Conservar ao abrigo de luz",
+            },
+            {
+                "id": 2,
+                "nome_do_produto": "CADEIRA",
+                "nome_da_empresa": "Forces of Nature",
+                "data_de_fabricacao": "2022-07-04",
+                "data_de_validade": "2023-09-09",
+                "numero_de_serie": "LD123",
+                "instrucoes_de_armazenamento": "Conservar ao abrigo de luz",
+            },
+            {
+                "id": 3,
+                "nome_do_produto": "GARRAFA",
+                "nome_da_empresa": "My company",
+                "data_de_fabricacao": "2022-08-01",
+                "data_de_validade": "2023-10-01",
+                "numero_de_serie": "4152",
+                "instrucoes_de_armazenamento": "Não amassar",
+            },
+        ]
+    )
+    assert (
+        "\033[32mData de fabricação mais antiga:\033[0m" in simple_report_test
+    )
+    assert (
+        "\033[32mData de validade mais próxima:\033[0m" in simple_report_test
+    )
+    assert "\033[32mEmpresa com mais produtos:\033[0m" in simple_report_test
+    assert "\033[36m2022-05-04\033[0m" in simple_report_test
+    assert "\033[36m2023-02-09\033[0m" in simple_report_test
+    assert "\033[31mForces of Nature\033[0m" in simple_report_test
